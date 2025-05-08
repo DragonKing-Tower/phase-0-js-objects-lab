fcd43c7fb3c73e3cce9e4e34489c61017a604503
Cyril Rafalovich <replanttrees@gmail.com>
2025-05-07 14:47:54 -1000

    This is the 3rd lab for module 5 Section 1.

--- index.js
+++ index.js
@@ -1,6 +1,47 @@
 //Write your code here
+//Attemdee and properties
+const attendee = {
+  attendeeId: "T001",
+  name: "Alice Smith",
+  event: "JavaScript Conference",
+  ticketType: "VIP",
+  ticketPrice: 150.00
+}
 
+//Function to log attendees
+function logAttendeeName(attendee){
+  console.log(attendee.name)
+}
 
+//Function to log price
+
+function logTicketPrice(attendee){
+  console.log(attendee.ticketPrice)
+}
+
+//Function to update ticket type
+
+function updateTicketType(attendee,newTicketType){
+  attendee.ticketType = newTicketType
+}
+
+//Function to update ticket price
+
+function updateTicketPrice(attendee,newTicketPrice){
+  attendee.ticketPrice = newTicketPrice
+}
+
+//Function to remove event
+
+function removeEventProperty(attendee){
+  delete attendee["event"]
+}
+
+//Function to add event
+
+function addCheckedInProperty(attendee){
+  attendee.checkedIn = true
+}
 
 //Needed for the tests to work. Don't modify
 module.exports = {
